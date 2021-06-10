a2f0f00909cc27186c9e0515f35f505d13f470b1
tolentinoel <rckrleyn@gmail.com>
2021-06-09 19:16:29 -0700

    error on fetching favicon.ico

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -2,16 +2,23 @@ import React from "react";
 import "../styles/DarkMode.css";
 
 const DarkMode = () => {
-  let clickedClass = "clicked";
-  const body = document.body;
-  const lightTheme = "light";
-  const darkTheme = "dark";
+  let clickedClass = "clicked";  //className to be added on the BUTTON to determine the mode
+  const body = document.body; //select the body so that we can change styling when button is clicked
+  const lightTheme = "light"; //className to be added on the BODY element to determine the mode
+  const darkTheme = "dark"; //className to be added on the BODY element to determine the mode
   let theme
 
+  //Could've used state but want to try new way to persist theme.
+  // localStorage - browser internal memory, unlimited capacity, since didn't need high-security
+  // with toggling theme, only a single property value.
+
+// used if statement to set value of theme 
   if (localStorage) {
-    theme = localStorage.getItem("theme")
+    theme = localStorage.getItem("theme") 
   }
 
+  //if statement to check whether theme is currently light or dark,
+  //then add className to which it corresponds
   if (theme === lightTheme || theme === darkTheme) {
     body.classList.add(theme)
   } else {
